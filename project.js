d5614b388295baf6fe94212e5b76808f804a4a66
Ömer Faruk Tektaş <omertektas0824@gmail.com>
2024-11-01 16:14:05 +0300

    a3

--- project.js
+++ project.js
@@ -3,14 +3,23 @@ const form = document.getElementById("film-form");
 const titleElement = document.querySelector("#title");
 const directorElement = document.querySelector('#director');
 const urlElement = document.querySelector('#url');
+const cardBody = document.querySelectorAll(".card-body")[1];
 
 const ui = new UI(); //UI objesini başlatma
+
+const storage = new Storage();
 
 //TÜm eventleri yükleme
 eventListeners();
 
 function eventListeners(){
     form.addEventListener("submit",addFilm);
+    document.addEventListener("DOMContentLoaded",function(){ // sayfa yüklendiğinde bunu kullanabilirsin
+        console.log("selam");
+        let films = storage.getFilmFromStorage();
+        ui.loadAllFilms(films);
+    })
+    cardBody.addEventListener("click",deleteFilm);
 }
 
 function addFilm(e){
@@ -25,8 +34,17 @@ function addFilm(e){
         // yeni film
         const newMovie = new Movie(title,director,url);
         ui.addFilmToUI(newMovie);// Arayüze film ekleme
-        ui.displayMessages("Film başarıyla eklendi","success")
+        ui.displayMessages("Film başarıyla eklendi","success");
+        storage.addFilmToStorage(newMovie);
+        console.log(newMovie);
     }
     ui.clearInputs(titleElement,directorElement,urlElement);
     e.preventDefault();
+}
+
+function deleteFilm(e){
+    if(e.target.id === "delete-film"){
+        ui.deleteFilmFromUI(e.target); 
+        storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling);
+    }
 }
--- ui.js
+++ ui.js
@@ -35,3 +35,25 @@ UI.prototype.displayMessages = function(message,type){
         div.remove();
     },1000); 
 }
+
+UI.prototype.loadAllFilms = function(films){
+    
+    const filmList = document.getElementById("films");
+
+    films.forEach(function(film){
+        filmList.innerHTML += `
+         <tr>
+               <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+               <td>${film.title}</td>
+               <td>${film.director}</td>
+               <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+         </tr> 
+        `
+    });
+
+}
+
+UI.prototype.deleteFilmFromUI = function(element){
+
+    element.parentElement.parentElement.remove();
+}
